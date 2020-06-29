53f652530ca95575882c16c1b46db3deb4f72141
Giovanni Bauermeister <giovanni.bauermeister@gmail.com>
2020-06-29 19:10:49 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        onPress={() => Alert.alert('Hello World!')}
+        title="Press me"
+        color="#4dc3ff"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
